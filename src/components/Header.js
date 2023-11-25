c100980cc53518a62c9434371364f50beeaa4dac
Dexter Ty <dexter.gets@gmail.com>
2023-11-25 08:28:49 -0500

    Header and Footer Links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,49 @@
-import { NavbarBrand, Navbar } from "reactstrap";
+import { useState } from "react";
+import { NavbarBrand, Navbar, Collapse, NavbarToggler,Nav,NavItem } from "reactstrap";
 import NucampLogo from '../app/assets/img/logo.png';
+import { NavLink } from "react-router-dom";
+
 
 const Header = () => {
+    const [menuOpen, setMenueOpen] = useState(false);
+
+
     return (
 
             <Navbar dark color='primary' sticky='top' expand='md'>
                     <NavbarBrand href='/'>
-                        <img src={NucampLogo} alt='nucamp logo' />
+                        <img src={NucampLogo} alt='nucamp logo' className='float-start'/>
+                        <h1>NuCamp</h1>
                     </NavbarBrand>
+
+                    <NavbarToggler onClick={() => setMenueOpen(!menuOpen)} />
+                    <Collapse isOpen={menuOpen} navbar>
+                        <Nav className='ms-auto' navbar>
+                            <NavItem>
+                                <NavLink className='nav-link' to='/'>
+                                    <i className='fa fa-home fa-lg'/> Home 
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className='nav-link' to='/directory'>
+                                    <i className='fa fa-list fa-lg'/> Directory
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className='nav-link' to='/about'>
+                                    <i className='fa fa-info fa-lg'/> About
+
+                                </NavLink>
+                                
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className='nav-link' to='/contact'>
+                                    <i className='fa fa-address fa-lg'/> Contact
+
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>  
             </Navbar>
     )
 }
