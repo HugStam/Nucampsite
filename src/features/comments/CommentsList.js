1fe5e81e2382dd4d84726b6cdff79a2d8963e985
Dexter Ty <dexter.gets@gmail.com>
2023-12-10 00:05:03 -0500

    addComment

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -4,14 +4,15 @@ import Comment from './Comment';
 import { selectCommentsByCampsiteId } from './commentsSlice';
 import CommentForm from  './CommentForm'
 
-const CommentsList =({campsiteId}) => {
-    const comment = useSelector(selectCommentsByCampsiteId(campsiteId));
 
-    if (comment && comment.length > 0) {
+const CommentsList =({campsiteId}) => {
+    const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
+
+    if (comments && comments.length > 0) {
         return (
             <Col md='5' className='m-1'>
                 <h4>Comments</h4>
-                {comment.map((comment) => {
+                {comments.map((comment) => {
                     return <Comment key={comment.id} comment={comment} />
                 })}
                 <CommentForm campsiteId={campsiteId}/>
